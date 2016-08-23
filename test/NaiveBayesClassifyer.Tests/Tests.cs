e048b171e7e8897b305c1d73f068aa5d10b3151a
KDCLLC <info@kingdavidconsulting.com>
2016-08-23 14:55:01 -0400

    add unit test support

---
+++ test/NaiveBayesClassifyer.Tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using MachineLearning;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TestGenerateSample(){
+            
+            var gen =  GenerateSample.MakeData(40);
+
+            Assert.Equal(40,gen.Length);
+        }
+    }
+}
